9fd8d884724de5ebf9ae06c798e45aa514f6f9e0
alannotnerd <alan1995wang@outlook.com>
2023-02-20 18:19:01 +0800

    feat: use shared memory (#4)

--- halo2_proofs/src/cuda/msm.cu
+++ halo2_proofs/src/cuda/msm.cu
@@ -1,12 +1,26 @@
 #include <cuda.h>
-extern "C" __constant__ int my_constant = 314;
-extern "C" __global__ void make_buckets(const uint32_t *bases,
-                                        const uint32_t *scalars,
-                                        uint32_t *buckets, int buf_len) {
+
+#define BUCKET_LEN 256
+
+__device__ auto make_buckets_shared(const uint32_t *bases,
+                                    const uint32_t *scalars, uint32_t *buckets,
+                                    int buf_len) {
+  __shared__ uint32_t s_buckets[BUCKET_LEN];
   auto tid = threadIdx.x;
   for (auto i = 0; i < buf_len; i++) {
     if (tid == scalars[i]) {
-      buckets[tid] += bases[i];
+      s_buckets[tid] += bases[i];
     }
   }
+  __syncthreads();
+  if (tid == 0) {
+    memcpy(buckets, s_buckets, BUCKET_LEN * sizeof(uint32_t));
+  }
+  __syncthreads();
+}
+
+extern "C" __global__ void make_buckets(const uint32_t *bases,
+                                        const uint32_t *scalars,
+                                        uint32_t *buckets, int buf_len) {
+  make_buckets_shared(bases, scalars, buckets, buf_len);
 }
